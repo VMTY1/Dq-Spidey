319b7eaace311038fa3b4b319042ec416cec1340
Swami <146053559+VMTY1@users.noreply.github.com>
2023-11-30 19:23:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Dq-Spidey
 fi
-cd /PETER-PARKER-BOT
+cd /Dq-Spidey
 pip3 install -U -r requirements.txt
 echo "Starting Bot..."
 python3 bot.py
